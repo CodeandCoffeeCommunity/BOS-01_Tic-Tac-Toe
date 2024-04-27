c38174f902363298d43be1d8c0b86a55b14cd7ae
Thomas Eykamp <embergameslp@gmail.com>
2024-04-26 22:06:38 -0400

    basic cell component

---
+++ src/components/Cell.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    {{ props.displayValue }}
+  </div>
+</template>
+
+<script setup lang="ts">
+type Marker = 'X' | 'O' | null
+
+const props = defineProps<{
+  displayValue: Marker
+}>()
+</script>
