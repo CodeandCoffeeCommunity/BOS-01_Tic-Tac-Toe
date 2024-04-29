af8d06d6ae55111aed1e498738ba041d1a196c71
Thomas Eykamp <embergameslp@gmail.com>
2024-04-28 23:29:18 -0400

    stylize the game

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,35 @@
 <template>
-  <div>{{ currentPlayer }}</div>
-  _
-  <Cell 
-    v-for="(cell, index) in board"
-    :display-value="cell"
-    @click="playMove(index)"
-  />
+  winner: {{ isGameOver ? ( computeWinner ? currentPlayer : 'Tie') : 'Game Ongoing' }}
+  <div :style="`display: flex; flex-wrap: wrap; gap: ${SQUARE_GAP}px; width: ${boardSize * (SQUARE_GAP * 2 + SQUARE_SIZE)}px;`">
+    <Cell 
+      v-for="(cell, index) in board" 
+      :display-value="cell"
+      :size="SQUARE_SIZE"
+      @click="playMove(index)" 
+    />
+  </div>
+  <input 
+    v-model="boardSize"
+    type="range" 
+    id="slider" 
+    name="slider" 
+    min="3" 
+    max="10"
+  >
+
 </template>
 
 <script setup lang="ts">
 import Cell from './components/Cell.vue'
-
+import { computed, ref } from 'vue'
 import { useBoardController } from './composables/useBoardController'
 
-const BOARD_SIZE = 5
+const boardSize = ref(3)
 
-const { board, currentPlayer, playMove } = useBoardController(BOARD_SIZE)
+const SQUARE_SIZE = 50
+const SQUARE_GAP = 3
+
+const { board, currentPlayer, playMove, computeWinner, computeIsBoardFull } = useBoardController(boardSize)
+
+const isGameOver = computed(() => computeWinner.value || computeIsBoardFull.value)
 </script>
--- src/components/Cell.vue
+++ src/components/Cell.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    {{ props.displayValue === '' ? 'empty' : props.displayValue}}
+  <div :style="`width: ${size}px; height: ${size}px; background-color: rgb(25, 25, 25); display: flex; align-items: center; justify-content: center; border-radius: 5px; font-style: black; font-size: larger; cursor: ${props.displayValue ? 'not-allowed' : 'pointer'};`">
+    {{ props.displayValue ?? props.displayValue}}
   </div>
 </template>
 
@@ -8,6 +8,7 @@
 import type { Marker } from '../composables/useBoardController'
 
 const props = defineProps<{
-  displayValue: Marker
+  displayValue: Marker,
+  size: number,
 }>()
 </script>
